25265c054531ab458f8df9a12c23c454717ed45a
Dâmaris <camposdamaris08@gmail.com>
2024-03-01 16:34:20 -0400

    Cria Tela de Login

--- app/src/main/java/edu/damaris/queridodiario/HomeActivity.java
+++ app/src/main/java/edu/damaris/queridodiario/HomeActivity.java
@@ -1,6 +1,9 @@
 package edu.damaris.queridodiario;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +12,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class HomeActivity extends AppCompatActivity {
-
+    Button buttonCriarDiario;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        buttonCriarDiario = findViewById(R.id.buttonHome);
+//        buttonCriarDiario.setBackgroundColor(getResources().getColor(R.color.contrast));
+
+        buttonCriarDiario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {irParaTelaLogin();}
+        });
+    }
+    public void irParaTelaLogin(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
