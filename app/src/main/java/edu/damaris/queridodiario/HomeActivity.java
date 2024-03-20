e80866fb505a3040a7c3ec979edb302660ca394f
Dâmaris <camposdamaris08@gmail.com>
2024-03-19 21:48:20 -0400

    Modifica telas de login e home

--- app/src/main/java/edu/damaris/queridodiario/HomeActivity.java
+++ app/src/main/java/edu/damaris/queridodiario/HomeActivity.java
@@ -15,18 +15,21 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
-    Button buttonCriarDiario;
-    EditText usuario, senha, repetirSenha;
-    DataBase dataBase;
+    private Button buttonCriarDiario;
+    private EditText usuario, senha, repetirSenha;
+    private TextView login;
+    private DataBase dataBase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
         buttonCriarDiario = findViewById(R.id.buttonHome);
         usuario = findViewById(R.id.editTextUsuario);
         senha = findViewById(R.id.editTextSenha);
         repetirSenha = findViewById(R.id.editTextRepetirSenha);
+        login = findViewById(R.id.TextViewLogin);
         dataBase = new DataBase(this);
 
         buttonCriarDiario.setOnClickListener(new View.OnClickListener() {
@@ -36,65 +39,49 @@ public class HomeActivity extends AppCompatActivity {
                 String senha1 = senha.getText().toString().trim();
                 String senha2 = repetirSenha.getText().toString().trim();
 
+                boolean usuarioExiste = dataBase.verificarUsuario(nome);
+                boolean inseridoComSucesso = dataBase.inserirDadosUsuario(nome, senha1);
+
                 if (TextUtils.isEmpty(nome) || TextUtils.isEmpty(senha1) || TextUtils.isEmpty(senha2)) {
-                    LayoutInflater layoutInflater = getLayoutInflater();
-                    View layout = layoutInflater.inflate(R.layout.custom_toast, findViewById(R.id.containerToast));
+                    toast(R.drawable.error__4_, R.string.campos_vazios);
+                } else if (!senha1.equals(senha2)) {
+                    toast(R.drawable.error__4_, R.string.senhas_diferentes);
+                }else if (usuarioExiste){
+                    toast(R.drawable.error__4_, R.string.usuario_existente);
+                }else if (inseridoComSucesso){
+                    toast(R.drawable.check, R.string.sucesso);
 
-                    ImageView imageView = layout.findViewById(R.id.toastError);
-                    imageView.setImageResource(R.drawable.error__4_);
-
-                    TextView textView = layout.findViewById(R.id.textToast);
-                    textView.setText(R.string.campos_vazios);
-
-                    Toast toast = new Toast(getApplicationContext());
-                    toast.setDuration(Toast.LENGTH_SHORT);
-                    toast.setView(layout);
-                    toast.show();
+                    Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+                    startActivity(intent);
                 } else {
-                    if (!senha1.equals(senha2)) {
-                        LayoutInflater layoutInflater = getLayoutInflater();
-
-                        View layout = layoutInflater.inflate(R.layout.custom_toast2, findViewById(R.id.containerToast2));
-
-                        ImageView imageView = layout.findViewById(R.id.toastError);
-                        imageView.setImageResource(R.drawable.error__4_);
-
-                        TextView textView = layout.findViewById(R.id.textToast2);
-                        textView.setText(R.string.senhas_diferentes);
-
-                        Toast toast = new Toast(getApplicationContext());
-                        toast.setDuration(Toast.LENGTH_SHORT);
-                        toast.setView(layout);
-                        toast.show();
-                    }else {
-                        boolean usuarioExiste = dataBase.verificarUsuario(nome);
-                        if (usuarioExiste){
-                            Toast.makeText(HomeActivity.this, "Usuario já existe!", Toast.LENGTH_SHORT).show();
-                        }else {
-                            boolean inseridoComSucesso = dataBase.inserirDadosUsuario(nome, senha1);
-                            if (inseridoComSucesso){
-                                LayoutInflater layoutInflater1 = getLayoutInflater();
-                                View layout1 = layoutInflater1.inflate(R.layout.custom_toast3, findViewById(R.id.containerToast3));
-
-                                ImageView imageView1 = layout1.findViewById(R.id.toastSucesso);
-                                imageView1.setImageResource(R.drawable.check);
-
-                                TextView textView1 = layout1.findViewById(R.id.textToast3);
-                                textView1.setText(R.string.sucesso);
-
-                                Toast toast1 = new Toast(getApplicationContext());
-                                toast1.setDuration(Toast.LENGTH_SHORT);
-                                toast1.setView(layout1);
-                                toast1.show();
-
-                                Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
-                                startActivity(intent);
-                            }
-                            else {
-                                Toast.makeText(HomeActivity.this, "Erro ao inserir usuario!", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    }
-
+                    toast(R.drawable.error__4_, R.string.erro_ao_inserir);
+                }
+            };});
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                irParaTelaLogin();{
+                }
+            }
+        });
     }
-};});}}
+    private void toast(int imageResource, int textResource){
+        LayoutInflater layoutInflater = getLayoutInflater();
+        View layout = layoutInflater.inflate(R.layout.custom_toast, findViewById(R.id.containerToast));
+
+        ImageView imageView = layout.findViewById(R.id.toast);
+        imageView.setImageResource(imageResource);
+
+        TextView textView = layout.findViewById(R.id.textToast);
+        textView.setText(textResource);
+
+        Toast toast = new Toast(getApplicationContext());
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+        toast.show();
+    }
+    private void irParaTelaLogin(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/edu/damaris/queridodiario/LoginActivity.java
+++ app/src/main/java/edu/damaris/queridodiario/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +14,14 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
     Button login;
+    private TextView criarCadastro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         login = findViewById(R.id.buttonLogin);
+        criarCadastro = findViewById(R.id.TextViewNovoDiario);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,9 +29,20 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+        criarCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                irParaTelaHome();{
+                }
+            }
+        });
     }
     public void irParaTelaPage(){
         Intent intent = new Intent(this, PageActivity.class);
+        startActivity(intent);
+    }
+    private void irParaTelaHome(){
+        Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
     }
 }
